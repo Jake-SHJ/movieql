cc5dfb8a19e3d0bc989d687ea46bb41de353ee9e
jake <guswns1853@gmail.com>
2020-08-12 00:15:23 +0900

    더 복잡한 schema의 작성 및 arg를 받는 query 작성

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,33 @@
+const people = [
+  {
+    id: "0",
+    name: "Jake",
+    age: 29,
+    gender: "male",
+  },
+  {
+    id: "1",
+    name: "Tom",
+    age: 29,
+    gender: "male",
+  },
+  {
+    id: "2",
+    name: "Vicky",
+    age: 31,
+    gender: "female",
+  },
+];
+
+const getById = (id) => {
+  const filteredPeople = people.filter((person) => person.id === String(id));
+  return filteredPeople[0];
+};
+
 const resolvers = {
   Query: {
-    name: () => "Jake",
+    people: () => people,
+    person: (_, { id }) => getById(id),
   },
 };
 
